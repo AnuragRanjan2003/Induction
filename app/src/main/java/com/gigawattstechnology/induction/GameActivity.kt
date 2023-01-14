b63c0714a53739e699f45215043153b889029120
AnuragRanjan2003 <anuragranjan1261@gmail.com>
2023-01-14 12:05:13 +0530

    ui updated

---
+++ app/src/main/java/com/gigawattstechnology/induction/GameActivity.kt
@@ -0,0 +1,66 @@
+package com.gigawattstechnology.induction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+        val chosenCard=intent.getStringExtra("Chosen")
+        //Toast.makeText(this,chosenCard,Toast.LENGTH_LONG).show()
+        val number= chosenCard?.toInt()
+        var trials:Int=3
+        var winCount:Int=0
+        var imageCard: ImageView =findViewById(R.id.Card_start)
+        val trialsLeft:TextView=findViewById(R.id.Trials)
+        val wins:TextView=findViewById(R.id.Wins)
+        val button=findViewById<Button>(R.id.FlipCard)
+        button.setOnClickListener {
+            button.text="Flip Again"
+            if (trials>0){
+                var random:Int=(1..13).random()
+                trials=trials-1
+                trialsLeft.text=""+trials
+                if(random==number){
+                    winCount++
+                    wins.text=""+winCount
+                }
+                val cardID=when(random){
+                    1->R.drawable.card1
+                    2->R.drawable.card2
+                    3->R.drawable.card3
+                    4->R.drawable.card4
+                    5->R.drawable.card5
+                    6->R.drawable.card6
+                    7->R.drawable.card7
+                    8->R.drawable.card8
+                    9->R.drawable.card9
+                    10->R.drawable.card10
+                    11->R.drawable.card11
+                    12->R.drawable.card12
+                    else->R.drawable.card13
+                }
+                imageCard.setImageResource(cardID)
+                if(trials==0){
+                    if (winCount.compareTo(2)>=0){
+                        button.text="You Won"
+                        Toast.makeText(this,"You Win",Toast.LENGTH_LONG).show()
+                    }else{
+                        button.text="You Loose"
+                        Toast.makeText(this,"You Loose",Toast.LENGTH_LONG).show()
+                    }
+                }
+            }else{
+                if (winCount.compareTo(2)>=0){
+                    button.text="You Won"
+                    Toast.makeText(this,"You Win",Toast.LENGTH_LONG).show()
+                }else{
+                    button.text="You Loose"
+                    Toast.makeText(this,"You Loose",Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/gigawattstechnology/induction/MainActivity.kt
@@ -0,0 +1,112 @@
+package com.gigawattstechnology.induction
+
+import android.content.DialogInterface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.RadioButton
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val choose:Button=findViewById(R.id.Choose)
+        choose.setOnClickListener {
+            val alertDialog: AlertDialog.Builder =AlertDialog.Builder(this)
+            val dialogView: View =LayoutInflater.from(this).inflate(R.layout.dialog_view,null)
+
+
+
+            val R1:RadioButton=dialogView.findViewById(R.id.Radio1)
+            val R2:RadioButton=dialogView.findViewById(R.id.Radio2)
+            val R3:RadioButton=dialogView.findViewById(R.id.Radio3)
+            val R4:RadioButton=dialogView.findViewById(R.id.Radio4)
+            val R5:RadioButton=dialogView.findViewById(R.id.Radio5)
+            val R6:RadioButton=dialogView.findViewById(R.id.Radio6)
+            val R7:RadioButton=dialogView.findViewById(R.id.Radio7)
+            val R8:RadioButton=dialogView.findViewById(R.id.Radio8)
+            val R9:RadioButton=dialogView.findViewById(R.id.Radio9)
+            val R10:RadioButton=dialogView.findViewById(R.id.Radio10)
+            val R11:RadioButton=dialogView.findViewById(R.id.Radio11)
+            val R12:RadioButton=dialogView.findViewById(R.id.Radio12)
+            val R13:RadioButton=dialogView.findViewById(R.id.Radio13)
+
+            alertDialog.setView(dialogView).setPositiveButton("Start Game",
+                DialogInterface.OnClickListener { dialog, which ->
+                    if (R1.isChecked){
+                        val intent=Intent(this,GameActivity::class.java)
+                        intent.putExtra("Chosen","1")
+                        startActivity(intent)
+                    }
+                    if (R2.isChecked){
+                        val intent=Intent(this,GameActivity::class.java)
+                        intent.putExtra("Chosen","2")
+                        startActivity(intent)
+                    }
+                    if (R3.isChecked){
+                        val intent=Intent(this,GameActivity::class.java)
+                        intent.putExtra("Chosen","3")
+                        startActivity(intent)
+                    }
+                    if (R4.isChecked){
+                        val intent=Intent(this,GameActivity::class.java)
+                        intent.putExtra("Chosen","4")
+                        startActivity(intent)
+                    }
+                    if (R5.isChecked){
+                        val intent=Intent(this,GameActivity::class.java)
+                        intent.putExtra("Chosen","5")
+                        startActivity(intent)
+                    }
+                    if (R6.isChecked){
+                        val intent=Intent(this,GameActivity::class.java)
+                        intent.putExtra("Chosen","6")
+                        startActivity(intent)
+                    }
+                    if (R7.isChecked){
+                        val intent=Intent(this,GameActivity::class.java)
+                        intent.putExtra("Chosen","7")
+                        startActivity(intent)
+                    }
+                    if (R8.isChecked){
+                        val intent=Intent(this,GameActivity::class.java)
+                        intent.putExtra("Chosen","8")
+                        startActivity(intent)
+                    }
+                    if (R9.isChecked){
+                        val intent=Intent(this,GameActivity::class.java)
+                        intent.putExtra("Chosen","9")
+                        startActivity(intent)
+                    }
+                    if (R10.isChecked){
+                        val intent=Intent(this,GameActivity::class.java)
+                        intent.putExtra("Chosen","10")
+                        startActivity(intent)
+                    }
+                    if (R11.isChecked){
+                        val intent=Intent(this,GameActivity::class.java)
+                        intent.putExtra("Chosen","11")
+                        startActivity(intent)
+                    }
+                    if (R12.isChecked){
+                        val intent=Intent(this,GameActivity::class.java)
+                        intent.putExtra("Chosen","12")
+                        startActivity(intent)
+                    }
+                    if (R13.isChecked){
+                        val intent=Intent(this,GameActivity::class.java)
+                        intent.putExtra("Chosen","13")
+                        startActivity(intent)
+                    }
+                }
+            )
+            alertDialog.show()
+        }
+    }
+}
+
+
